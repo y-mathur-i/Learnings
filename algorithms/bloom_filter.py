ebdf8a49bdc2edb01c74c6a0a0093fbecf712ab7
Yogesh Mathur <69184665+y-mathur-i@users.noreply.github.com>
2023-04-04 18:59:04 +0000

    BLOOM FILTER BASE

---
+++ algorithms/bloom_filter.py
@@ -0,0 +1,20 @@
+"""Module with sample implementation for the bloom filter
+   Which is a probabilistic data structure
+"""
+
+class BloomFilter:
+    """Bloom filter class
+    """
+    def __init__(self) -> None:
+        pass
+
+    def search(self, key) -> bool:
+        """Method to check if the key exists .i.e was visited again
+        """
+    
+    def add(self, key) -> None:
+        """Method to add key to bloom filter
+        """
+
+if __name__=="__main__":
+    bloom_filter = BloomFilter()
