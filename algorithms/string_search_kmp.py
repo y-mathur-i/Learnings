5444e3f51d99324eedadddc876354d285fc851d2
Yogesh Mathur <69184665+y-mathur-i@users.noreply.github.com>
2023-04-09 18:26:12 +0000

    save

--- algorithms/string_search_kmp.py
+++ algorithms/string_search_kmp.py
@@ -5,7 +5,7 @@
 def search_string(string_one: str, string_two: str) -> int:
     """Method to search for string_two in string_one
         usign kmp algorithm and return the first index it shows up on.
-    """ 
+    """
     assert len(string_one) >= len(string_two)
     # my understanding
     # 1. we for each point in s1 know the lenght of the longest proper prefix for s2
