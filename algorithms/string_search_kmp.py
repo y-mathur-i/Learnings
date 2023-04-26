9e455345cf57f8eb55828001cd18362dc53cc3c1
Yogesh Mathur <69184665+y-mathur-i@users.noreply.github.com>
2023-04-26 19:22:23 +0000

    kmp fix

--- algorithms/string_search_kmp.py
+++ algorithms/string_search_kmp.py
@@ -27,20 +27,20 @@ def search_string(string_one: str, pattern: str) -> int:
 
     """
     assert len(string_one) >= len(pattern)
-    lps_arr = create_lps_array(string=string_one)
+    lps_arr = create_lps_array(string=pattern)
     s_idx, p_idx  = 0, 0
     while s_idx < len(string_one):
         if string_one[s_idx] == pattern[p_idx]:
             s_idx, p_idx = s_idx + 1, p_idx + 1
         else:
             if p_idx != 0:
-                p_idx = lps_arr[s_idx-1]
+                p_idx = lps_arr[p_idx-1]  # at lps[s_idx-1] there is a proper prefix that is also a proper suffix
             s_idx += 1
         if p_idx == len(pattern):
-            return p_idx - len(pattern)
+            return s_idx - len(pattern)
     return -1
 
 
 if __name__ == "__main__":
-    assert search_string("abcd", "ab") == 0
+    assert search_string("absdcdsdcdcd", "cdcd") == 8
     assert search_string("abcd", "skjh") == -1
