d650a4679e700c553d4a3094891c1dddfad79a67
Yogesh Mathur <69184665+y-mathur-i@users.noreply.github.com>
2023-04-26 19:14:21 +0000

    added kmp algo

--- algorithms/string_search_kmp.py
+++ algorithms/string_search_kmp.py
@@ -1,20 +1,46 @@
 """Module with kmp algorithm for string searching
 """
+from typing import List
 
 
-def search_string(string_one: str, string_two: str) -> int:
+def create_lps_array(string: str) -> List[int]:
+    """Method to generate lps array
+    """
+    arr = [0]*len(string)
+    right, left = 1, 0
+    while right < len(string):
+        if string[left] == string[right]:
+            left += 1
+        else:
+            left = 0
+        arr[right] = left
+        right += 1
+    return arr
+
+def search_string(string_one: str, pattern: str) -> int:
     """Method to search for string_two in string_one
         usign kmp algorithm and return the first index it shows up on.
-    """
-    assert len(string_one) >= len(string_two)
-    # my understanding
-    # 1. we for each point in s1 know the lenght of the longest proper prefix for s2
-    # that ends there.
+        proper prefix & proper suffix
+        prefix and sufix that are subset of the string
+        longest proper prefix that is also a sufix
+        so we prepare a lps array where for each i we know a preffix that is also a suffux
 
+    """
+    assert len(string_one) >= len(pattern)
+    lps_arr = create_lps_array(string=string_one)
+    s_idx, p_idx  = 0, 0
+    while s_idx < len(string_one):
+        if string_one[s_idx] == pattern[p_idx]:
+            s_idx, p_idx = s_idx + 1, p_idx + 1
+        else:
+            if p_idx != 0:
+                p_idx = lps_arr[s_idx-1]
+            s_idx += 1
+        if p_idx == len(pattern):
+            return p_idx - len(pattern)
+    return -1
 
 
 if __name__ == "__main__":
-    string_one = "aaaaaabcb"
-    string_two = "aaaaabcsdf"
-    print(search_string(string_one, string_two))
- 
+    assert search_string("abcd", "ab") == 0
+    assert search_string("abcd", "skjh") == -1
